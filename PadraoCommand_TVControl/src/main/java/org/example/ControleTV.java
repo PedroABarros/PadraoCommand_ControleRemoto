c79801acb8bcb798053dc3af127a2864b7ccd188
PEDRO ABRAHAO BARROS <pedro.barros3@GRANBERY.local>
2024-11-13 20:31:52 -0300

    Implemantando na interface

--- PadraoCommand_TVControl/src/main/java/org/example/ControleTV.java
+++ PadraoCommand_TVControl/src/main/java/org/example/ControleTV.java
@@ -1,4 +1,5 @@
 package org.example;
 
-public class ControleTV {
+
+public class ControleTV implements{
 }
--- PadraoCommand_TVControl/src/main/java/org/example/TV.java
+++ PadraoCommand_TVControl/src/main/java/org/example/TV.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class TV {
+import java.util.ArrayList;
+import java.util.List;
+public class TV{
+        private List<FuncoesCommand> funcoes = new ArrayList<FuncoesCommand>();
+
+        public void executarFuncao(TV funcoes) {
+            this.funcoes.add(funcoes);
+            funcoes.executar();
+        }
 }
